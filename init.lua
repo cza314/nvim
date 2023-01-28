7121716882f20af2ec03def18188377ce4fad058
cza145 <zyh1032712227@outlook.com>
2023-01-28 15:05:14 +0800

    change comment

--- init.lua
+++ init.lua
@@ -1,12 +1,6 @@
--- 基本设置
 require('core.options')
--- 插件管理
-require('core.lazy').lazy_init()
--- 环境变量配置
+require('core.lazy').lazy_init() -- plugin manager
 require('core.environment')
--- 按键设置
 require('core.mapping')
--- 右键菜单
 require('core.menu')
--- theme
 require('core.theme')
