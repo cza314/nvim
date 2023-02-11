44c566514ae09c5e17f8a6df55ef48153ac57093
cza145 <zyh1032712227@outlook.com>
2023-02-11 12:47:25 +0800

    delete theme file

--- lua/core/theme.lua
+++
@@ -1,8 +0,0 @@
-local mode = require('core.utils').get_dark_mode()
-
-if mode == false then
-  vim.cmd('colorscheme ayu-light')
-  vim.cmd('colorscheme doom-one')
-else
-  vim.cmd('colorscheme hydrangea')
-end
