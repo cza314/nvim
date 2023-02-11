5b025de7bd60dd937f4991b6787990f60717781b
cza145 <zyh1032712227@outlook.com>
2023-02-11 20:27:21 +0800

    change theme function

--- init.lua
+++ init.lua
@@ -3,4 +3,9 @@ require('core.lazy').lazy_init() -- plugin manager
 require('core.environment')
 require('core.mapping')
 require('core.menu')
-require('core.utils').set_theme({'ayu-light', 'doom-one'}, {'hydrangea'})
+require('core.utils').set_theme({
+  'ayu-light',
+  'doom-one',
+}, {
+  'hydrangea'
+})
