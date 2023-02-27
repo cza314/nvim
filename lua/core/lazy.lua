87b4b56a17b28b92a2e709e4862d6687c8b6b741
cza314 <zyh1032712227@outlook.com>
2023-02-27 12:34:58 +0800

    add mapping for telescope

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -22,8 +22,15 @@ end
 
 function M.mapping()
   local set_map = require('core.utils').set_map
+  local theme = 'dropdown' -- theme for telescope
   set_map({
-    {'n', '<leader>b', '<CMD>NvimTreeToggle<CR>'}, -- nvim-tree
+    {'n', '<leader>ss','<CMD>Telescope builtin'     .. ' theme=' .. theme .. '<CR>'},
+    {'n', '<leader>sf','<CMD>Telescope find_files'  .. ' theme=' .. theme .. '<CR>'},
+    {'n', '<leader>sg','<CMD>Telescope live_grep'   .. ' theme=' .. theme .. '<CR>'},
+    {'n', '<leader>sb','<CMD>Telescope buffers'     .. ' theme=' .. theme .. '<CR>'},
+    {'n', '<leader>sh','<CMD>Telescope help_tags'   .. ' theme=' .. theme .. '<CR>'},
+    {'n', '<leader>so','<CMD>Telescope oldfiles'    .. ' theme=' .. theme .. '<CR>'},
+    {'n', '<leader>b', '<CMD>NvimTreeToggle<CR>'},
   })
 end
 
