38dd77024cbe146d309cec014fcb566fe0420f7d
cza314 <zyh1032712227@outlook.com>
2023-02-27 12:35:15 +0800

    lazy-load for telescope

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -14,17 +14,4 @@ function M.telescope()
   }
 end
 
-function M.mapping()
-  local set_map = require('core.utils').set_map
-  local theme = 'dropdown'
-  set_map({
-    {'n', '<leader>ss','<CMD>Telescope builtin'     .. ' theme=' .. theme .. '<CR>'},
-    {'n', '<leader>sf','<CMD>Telescope find_files'  .. ' theme=' .. theme .. '<CR>'},
-    {'n', '<leader>sg','<CMD>Telescope live_grep'   .. ' theme=' .. theme .. '<CR>'},
-    {'n', '<leader>sb','<CMD>Telescope buffers'     .. ' theme=' .. theme .. '<CR>'},
-    {'n', '<leader>sh','<CMD>Telescope help_tags'   .. ' theme=' .. theme .. '<CR>'},
-    {'n', '<leader>so','<CMD>Telescope oldfiles'    .. ' theme=' .. theme .. '<CR>'},
-  })
-end
-
 return M
