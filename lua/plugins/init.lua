7cb00dca6c8fd4ca97a06e2a61172c03ced3007b
cza314 <zyh1032712227@outlook.com>
2023-02-27 19:25:51 +0800

    add source for neovim lua api

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -221,6 +221,7 @@ return {
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-calc',
+      'hrsh7th/cmp-nvim-lua',
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
       'rafamadriz/friendly-snippets',
