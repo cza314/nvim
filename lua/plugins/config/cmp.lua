e561dbe3e23adb329855e30616b0a00223f15a65
cza314 <zyh1032712227@outlook.com>
2023-03-11 18:58:44 +0800

    update cmp trigger of length

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -32,6 +32,9 @@ function M.cmp()
   end
 
   local config = {
+    completion = {
+      keyword_length = 2,
+    },
     enabled = function()
       return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
     end,
